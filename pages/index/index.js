1917bf2bda52097f69b383cad1d07ed514549430
liuxiang7 <liuxiang7@tal.com>
2023-03-16 21:58:10 +0800

    feat：翻译功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,31 @@
-
+import transFunc from '../../utils/util.js'
 const app = getApp()
 
 Page({
   data: {
-    
+    query:'',// 用户输入的要翻译的文本
+    content : '', // 翻译的文本
   },
   // 跳转到选择语言
   gotoChange(){
     wx.navigateTo({
       url: '/pages/change/change',
     })
+  },
+  // 翻译事件
+  async translateHandler(){
+    // 1. 拿到用户输入的值  2. 调用接口进行翻译
+    try {
+      const content = await transFunc(this.data.query, "zh", "en")
+      this.setData({
+        content
+      })
+    } catch(err){
+      console.error(err)
+    }
+    
+  },
+  inputHandle(){
+
   }
 })
