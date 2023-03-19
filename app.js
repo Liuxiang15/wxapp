4ad1dae1788a6e4756d92059796f803d8fca89cf
liuxiang7 <liuxiang7@tal.com>
2023-03-19 20:58:47 +0800

    feat:任务清单和单条任务组件初步

--- app.js
+++ app.js
@@ -4,5 +4,13 @@ App({
    
   },
   globalData: {
+    // 存储所有的待办事项
+    list : [{
+      content : '看电影',
+      isComplete : true
+    },{
+      content : '学习小程序',
+      isComplete : false
+    }]
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,8 @@ const app = getApp()
 
 Page({
   data: {
-    
+    newContent : "", // 用户输入新的待办事项
+    list: app.globalData.list // 从全局 list 中获取所有任务
   },
-  
+  inputHandle(){}
 })
