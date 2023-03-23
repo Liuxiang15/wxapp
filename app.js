6bee49881c82c9c9d82b0ac3203f71a44f057472
liuxiang7 <liuxiang7@tal.com>
2023-03-23 23:16:35 +0800

    feat:发表

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-// app.js
 App({
-  onLaunch() {
-   
+  async onLaunch() {
+    // 初始化云服务
+    wx.cloud.init({
+      env: 'cloud1-3gytnxa2a4c59f07'
+    });
+    
   },
   globalData: {
+    userInfo: null
   }
 })
