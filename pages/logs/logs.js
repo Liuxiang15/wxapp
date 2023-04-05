5db50d51da8b1e1737a9ef23d9a8281e3d6619ad
liuxiang7 <liuxiang7@tal.com>
2023-04-05 20:27:53 +0800

    feat:本地日志

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,66 +1,21 @@
-// pages/logs/logs.js
+const {formatTime} = require("../../utils/util");
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    scanLogs : []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  onShow(){
+    // 从本地存储中获取 key 为 scanLogs 的存储记录
+    wx.getStorage({
+      key : "scanLogs",
+      success : res => {
+        this.setData({
+          scanLogs : (res.data || []).map(n => {
+            n.date = formatTime(new Date(n.date));
+            n.text = n.text.length > 10 ? n.text.slice(0,10) + "..." : n.text;
+            return n;
+          })
+        })
+      }
+    })
   }
 })
