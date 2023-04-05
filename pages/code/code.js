f88e13d2e6d68f4f388794d30e7ffd1d533bcb93
liuxiang7 <liuxiang7@tal.com>
2023-04-05 21:12:06 +0800

    feat:授权

--- pages/code/code.js
+++ pages/code/code.js
@@ -1,32 +1,68 @@
 Page({
   data: {
-    codeText : "", // 存储用户输入的二维码文本
-    imgTempFilePath : "", // 生成的二维码图片路径
+    codeText: "", // 存储用户输入的二维码文本
+    imgTempFilePath: "", // 生成的二维码图片路径
   },
   // 根据文本来生成二维码图片
-  onGenerate(){
+  onGenerate () {
     // 我们请求接口之后，会生成一张二维码的图片
     // 我们需要将这张图片下载下来
     wx.downloadFile({
-      url:"http://apis.juhe.cn/qrcode/api?key=5a4f1232be1db0388b7544ebee55e542&type=2&text="+this.data.codeText,
-      success : res => {
+      url: "http://apis.juhe.cn/qrcode/api?key=5a4f1232be1db0388b7544ebee55e542&type=2&text=" + this.data.codeText,
+      success: res => {
         this.setData({
-          imgTempFilePath : res.tempFilePath
+          imgTempFilePath: res.tempFilePath
         })
       }
     })
   },
-  saveImage(e){
+  saveImage (e) {
     const url = e.currentTarget.dataset.url
-    wx.saveImageToPhotosAlbum({
-       filePath:url,
-       success(res) { 
-        console.log(res);
-       },
-       fail(res){
-        console.log(res);
-       }
-    })
+    //用户需要授权
+    wx.getSetting({
+      success: (res) => {
+        // 没有授权就去要授权 
+        if (!res.authSetting['scope.writePhotosAlbum']) {
+          wx.authorize({
+            scope: 'scope.writePhotosAlbum',
+            success: () => {
+              // 同意授权
+              this.saveImg1(url);
+            },
+            fail: (res) => {
+              console.log(res);
+            }
+          })
+        } else {
+          // 已经授权了
+          this.saveImg1(url);
+        }
+      },
+      fail: (res) => {
+        console.log(res);
+      }
+    })
   },
-  bindInput(){}
+  saveImg1 (url) {
+    // wx.getImageInfo()可以获取网络路径的图片
+    wx.getImageInfo({
+      src: url,
+      success: (res) => {
+        let path = res.path;
+        wx.saveImageToPhotosAlbum({
+          filePath: path,
+          success: (res) => {
+            console.log(res);
+          },
+          fail: (res) => {
+            console.log(res);
+          }
+        })
+      },
+      fail: (res) => {
+        console.log(res);
+      }
+    })
+  },
+  bindInput () { }
 })
